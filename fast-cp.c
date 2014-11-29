af34076a60f926614c9353a8c84be15094a33290
Madhura Parikh <madhuraparikh@gmail.com>
2014-11-29 00:53:01 -0600

    works for directories too

--- fast-cp.c
+++ fast-cp.c
@@ -1,6 +1,8 @@
 // TODO arrange in alphabetical order, separated by type
-#include <stdlib.h>
-#include <stdio.h>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <string>
 #include <string.h>
 #include <signal.h>
 #include <stdint.h>
@@ -14,12 +16,15 @@
 #include <errno.h>
 
 #define BUF_MAX 128
+#define FD_MAX 1000
 
-size_t buffer_size;
-uint64_t  page_size;
-uint64_t num_pages;
-uint64_t num_requests;
-sem_t blocking_waiter;
+static size_t buffer_size;
+static uint64_t  page_size;
+static uint64_t num_pages;
+static uint64_t num_requests;
+static sem_t blocking_waiter;
+static std::string src;
+static std::string dst;
 
 void aio_write_handler(sigval_t signal);
 
@@ -97,7 +102,7 @@ void aio_write_handler (sigval_t sigval)
   //free(hctx->m_aiocb->aio_buf);
 }
 
-int copy_regular (const char* src, const char* dst)
+int copy_regular (const char* src_file, const char* dst_file)
 {
   int src_fd;
   int dst_fd;
@@ -105,22 +110,40 @@ int copy_regular (const char* src, const char* dst)
   void * buffer_block;
   // get the page_size for the system
   page_size = getpagesize();
-  struct stat stat_buf;
-  // open the source file for reading
-  if ((src_fd = open(src, O_RDONLY)) < 0) {
-    perror("source file open error");
-    exit(-1);
-  }
-  // stat the input file
-  if (fstat(src_fd, &stat_buf) < 0) {
+  struct stat stat_buf, stat_dst;
+  // stat the source file
+  if (stat(src_file, &stat_buf) < 0) {
     perror("source file stat error");
     exit(-1);
   }
+  // if its a directory, create and exit
+  if (S_ISDIR(stat_buf.st_mode)) {
+    if (mkdir(dst_file, S_IRWXU | S_IRWXG)) {
+      perror("mkdir error");
+      exit(-1);
+    }
+    return 0;
+  }
+  // open the source file for reading
+  if ((src_fd = open(src_file, O_RDONLY)) < 0) {
+    perror("source file open error");
+    exit(-1);
+  }
   // open the destination file for writing
-  if ((dst_fd = open(dst, O_WRONLY| O_CREAT, stat_buf.st_mode)) < 0) {
+  if ((dst_fd = open(dst_file, O_WRONLY| O_CREAT, stat_buf.st_mode)) < 0) {
+    std::cout << "file " <<dst_file<<std::endl;
     perror("destination file open error");
     exit(-1);
   }
+  if (fstat(dst_fd, &stat_dst)) {
+    perror("fstat destination error");
+    exit(-1);
+  }
+  // check if input and output are the same
+  if (stat_buf.st_dev  == stat_dst.st_dev && stat_buf.st_ino == stat_dst.st_ino) {
+    return 0;
+  }
+  
   // TODO tell the kernel that we will need the input file
   posix_fadvise(src_fd, 0, stat_buf.st_size, POSIX_FADV_WILLNEED);
   // more efficient space allocation via fallocate for dst file
@@ -172,23 +195,114 @@ int copy_regular (const char* src, const char* dst)
   return 0;
 }
 
-
-int copy_directory(
-		   const char* fpath, 
-		   const struct stat* sb, 
-		   int typeflag,
-		   struct FTW* ftwbuf)
+std::string split_filename(std::string fname, int depth) 
 {
+  uint64_t pos;
+  pos = fname.length();
+  if (fname.find_last_of("/\\", pos -1) == fname.length() - 1) {
+    --pos;
+  }
+  for (uint64_t i = 0; i < depth; ++i){
+    pos = fname.find_last_of("/\\", pos -1);
+  }
+  std::cout << "split " << fname.substr(pos) << std::endl;
+  return fname.substr(pos);
+}
 
+int tree_walk (const char* fpath, 
+	       const struct stat* sb, 
+	       int typeflag,
+	       struct FTW* ftwbuf)
+{
+  if (ftwbuf->level == 0) {
+    return 0;
+  }
+  std::string new_dst_path = dst + split_filename(std::string(fpath), ftwbuf->level);
+  copy_regular(fpath, new_dst_path.c_str());
+  return 0;
+}
 
+std::string format_path(std::string path) 
+{
+  uint64_t pos;
+  pos = path.find('/');
+  if (pos == path.length() -1 || pos == std::string::npos ) {
+    std::string fpath = "./";
+    fpath.append(path);
+    return fpath;
+  }
+  return path;  
 }
 
 int main(int argc, char * argv[])
 {
   num_requests = 0;
   sem_init(&blocking_waiter, 0, 0);
-  copy_regular(argv[1], argv[2]);
+  //copy_regular(argv[1], argv[2]);
+  src = argv[1];
+  dst = argv[2];
   uint64_t i;
+  src = format_path(src);
+  dst = format_path(dst);
+  struct stat src_stat, dst_stat;
+  if (stat(src.c_str(), &src_stat)) {
+    perror("source file stat error");
+    exit(-1);
+  }
+  if (stat(dst.c_str(), &dst_stat)) {
+    // if error, must be because of a no entry
+    if (errno != ENOENT) {
+      perror("destination file stat error");
+      exit(-1);
+    }
+    // new now check if we need to copy a file or directory
+    if (S_ISDIR(src_stat.st_mode)) {
+      // try creating the root at the destination
+      if(mkdir(dst.c_str(), S_IRWXU | S_IRWXG)) {
+	perror("destination mkdir failed");
+	exit(-1);
+      }
+      // traverse the entire tree and copy files or directories 
+      if (nftw(src.c_str(), tree_walk, FD_MAX, FTW_PHYS)) {
+	perror("nftw traversal error");
+	exit(-1);
+      }     
+    }
+    else { // is a file
+      copy_regular(src.c_str(), dst.c_str());
+    }
+  }
+  else { // dst already exists
+    if (S_ISDIR(src_stat.st_mode)) {
+      // if dir -> file error
+      if (!S_ISDIR(dst_stat.st_mode)) {
+	perror ("cannot copy directory to non-directory");
+	exit(-1);
+      }
+      // dir -> dir
+      dst.append(split_filename(src, 1));
+      std::cout <<"dst " << dst << "\n";
+      if (mkdir(dst.c_str(),  S_IRWXU | S_IRWXG)) {
+	perror("destination mkdir failed");
+	exit(-1);
+      }
+      if (nftw(src.c_str(), tree_walk, FD_MAX, FTW_PHYS)) {
+	perror("nftw traversal error");
+	exit(-1);
+      }
+    }
+    else {
+      if (!S_ISDIR(dst_stat.st_mode)) {
+	// file -> file overwrite
+	copy_regular(src.c_str(), dst.c_str());
+      }
+      else {
+	// file -> dir
+	dst.append(split_filename(src, 1));
+	copy_regular(src.c_str(), dst.c_str());
+      }
+    }
+  }
   for (i = 0; i < num_requests; ++i) {
     sem_wait(&blocking_waiter);
   }
